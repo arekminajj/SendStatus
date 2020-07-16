f6896df1902aaaf80aee66fdcb05bdbeb54ff02f
Arek Cios <arek123113@gmail.com>
2020-07-16 23:03:01 +0200

    Update status contoller and status modle

--- SendStatus/Controllers/StatusController.cs
+++ SendStatus/Controllers/StatusController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SendStatus.Services;
 
 namespace SendStatus.Controllers
 {
     public class StatusController : Controller
     {
-        public IActionResult Index()
+        private readonly IMongoService _service;
+        public StatusController(IMongoService service)
         {
+            _service = service;
+        }
+
+        public IActionResult Index(string lineOne, string lineTwo)
+        {
+            _service.SendUpdate(lineOne, lineTwo);
+
             return View();
         }
     }
---
+++ SendStatus/Models/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace SendStatus.Models
+{
+    public class Status
+    {
+        public string lineOne { get; set; }
+        public string lineTwo { get; set; }
+        public int Id { get; set; }
+    }
+}
